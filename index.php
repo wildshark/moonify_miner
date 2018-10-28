d7071b44898d01a8ca520825cd55a71b3272d3de
Andrew Quaye <andyquayegh@outlook.com>
2018-10-28 10:29:38 +0100

    add key
    add key

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
-<?php
-include("moonify.php"); //include the wrapper
+ <form action="http://api.moonify.io/0.2/moonitize/sessions" method="post" target="_blank">
+  First name: <input type="text" name="Authorization"><br>
 
-$moonify = new Moonify(); //declare a new moonify instance
-
-$moonify->set("serviceID","3lgIBtTCTuDIUZNu8Xy5xnncGPYxH7Xzf5w_ICJW-iaras8kzkCE3pzMDcxv1jgYpr8E5_WHqLfVz5zNKJj7u3N3sfG25NrgeIeV2HiRo1Pk21Ee2CcvzOaetxScR8k2"); //set your private ID as authentication
-
-$session​ ​=​ $moonify->session([​'userID'​=>​'MyUserID'​])->open(); //open a monetization session
-
-
-// First method :
-if($moonify->error){
-    echo $moonify->error; //show error
-} else {
-    echo $session->integrationCode(); //add Javascript code to set the monetization on client-side
-}
-
-// Second method : 
-if($moonify->error){
-    echo $moonify->error; //show error
-} else {
-    $tokenID = $session->tokenID();
-    echo '<script src="https://pkg.moonify.io/moonify.min.js"></script>';
-    echo '<script type="text/javascript">Moonify.set({tokenID : "'.$tokenID.'" });</script>';
-}
-?>
-
-<script type="text/javascript">        
-    Moonify.start();
-</script>
+  <input type="submit" value="Submit">
+</form> 
