2ba98540e1f91fc1a6a55566e5bc327cc220995b
Andrew Quaye <andyquayegh@outlook.com>
2018-10-28 06:47:37 +0100

    ok
    ok

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
+include("moonify.php"); //include the wrapper
 
-include("moonify.php"); //include the sdk
-$moonify=new Moonify(); //declare a new moonify instance
-$moonify->setParams(array(
-  "serviceID"=>"3lgIBtTCTuDIUZNu8Xy5xnncGPYxH7Xzf5w_ICJW-iaras8kzkCE3pzMDcxv1jgYpr8E5_WHqLfVz5zNKJj7u3N3sfG25NrgeIeV2HiRo1Pk21Ee2CcvzOaetxScR8k2", //Your private key
-  "userID"=>"1" //You can optionnaly pass the id of the user visiting your website 
-));
-$moonify->open(); //open the session
+$moonify = new Moonify(); //declare a new moonify instance
 
+$moonify->set("serviceID","3lgIBtTCTuDIUZNu8Xy5xnncGPYxH7Xzf5w_ICJW-iaras8kzkCE3pzMDcxv1jgYpr8E5_WHqLfVz5zNKJj7u3N3sfG25NrgeIeV2HiRo1Pk21Ee2CcvzOaetxScR8k2"); //set your private ID as authentication
+
+$session​ ​=​ $moonify->session([​'userID'​=>​'MyUserID'​])->open(); //open a monetization session
+
+
+// First method :
 if($moonify->error){
-	echo $moonify->error;
+    echo $moonify->error; //show error
 } else {
-	echo $moonify->getIntegrationCode();
+    echo $session->integrationCode(); //add Javascript code to set the monetization on client-side
+}
+
+// Second method : 
+if($moonify->error){
+    echo $moonify->error; //show error
+} else {
+    $tokenID = $session->tokenID();
+    echo '<script src="https://pkg.moonify.io/moonify.min.js"></script>';
+    echo '<script type="text/javascript">Moonify.set({tokenID : "'.$tokenID.'" });</script>';
 }
 ?>
